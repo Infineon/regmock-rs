58d2ae12d92d1979089a6f42bbf1d17bc75043a4
Andreas Wallner <20979738+andreasWallnerIFX@users.noreply.github.com>
2024-07-06 18:42:47 +0200

    Simplify given! macro for complete logs

--- examples/src/_4_sequence_match.rs
+++ examples/src/_4_sequence_match.rs
@@ -11,7 +11,7 @@ pub fn spi_send(bytes: &[u8]) {
 
 #[cfg(test)]
 mod tests {
-    use regmock_rs::utils::access_gen::write_value;
+    use regmock_rs::{given, require_reg, require_seq, utils::access_gen::write_value};
 
     use super::*;
     use crate::common::init_mock;
@@ -23,6 +23,19 @@ mod tests {
         // call our DUT
         spi_send(&[0x11, 0x22, 0x33]);
 
+        // use matcher
+        given!(
+            full_log,
+            require_reg!(SPI.tx(), values_written_are([0x11, 0x22, 0x33]))
+        );
+
+        // ... or using sequence matcher - could handle mix between registers/read&write
+        let w0 = write_value(SPI.tx().addr(), 0x11);
+        let w1 = write_value(SPI.tx().addr(), 0x22);
+        let w2 = write_value(SPI.tx().addr(), 0x33);
+        given!(full_log, require_seq!(vec![&w0, &w1, &w2]));
+
+        // ... or explicitly match - could handle filtering, etc.
         assert_eq!(
             regmock_rs::logs().iter().collect::<Vec<_>>(),
             vec![
--- src/matchers/macros.rs
+++ src/matchers/macros.rs
@@ -6,36 +6,62 @@
 /// The macro panics, if applying the `LogMatcher` to the [`RegisterAccess`](crate::utils::RegisterAccess)
 /// iterator fails.
 ///
+/// # Parameters
+///
+/// - access list, pass one of:
+///   - arbitrary iterater of register accesses, like `regmock_rs::log().iter()`, can be filtered etc.
+///   - `full_log` to match against complete log with duplicate accesses unrolled
+///   - `skip_log` to match against log with read accesses compressed (single entry for polling)
+///
 /// # Examples
 ///
-/// Best used together with `require_*!` macros to construct the
+/// Best used together with the [`require_reg!`] and [`require_seq!`] macros to construct the
 /// [`LogMatcher`](crate::matchers::LogMatcher)s.
 ///
 /// Check that register was written to only once.
 /// ```rust,ignore
-/// let log_iter = regmock_rs::get_logs().iter();
 /// given!(
-///     log_iter,
+///     full_log,
 ///     require_reg!(
-///         pac::PERIPHERAL.register().unwrap().addr(),
+///         pac::PERIPHERAL.register().unwrap(),
 ///         written_once
 ///     )
 /// );
 /// ```
 ///
-/// Check that sequnece of values was written to register.
+/// Check that sequence of values was written to register.
 /// ```rust,ignore
-/// let log_iter = regmock_rs::get_logs().unwrap().iter();
+/// let log_iter = regmock_rs::get_logs().iter();
 /// given!(
 ///     log_iter,
 ///     require_reg!(
-///         pac::PERIPHERAL.register().addr(),
-///         written_sequence(vec![0xAAAu32, 0xAABu32, 0xAACu32])
+///         pac::PERIPHERAL.register(),
+///         values_written_are(vec![0xAAAu32, 0xAABu32, 0xAACu32])
 ///     )
 /// );
 /// ```
 #[macro_export]
 macro_rules! given {
+    (full_log, $matcher: expr) => {{
+        use regmock_rs::matchers::*;
+        let mut m = $matcher;
+        match m.r#match(regmock_rs::logs().iter_full()) {
+            Ok(_) => ..,
+            Err(me) => {
+                panic!("\nFailed to match {} because:\n'{}'", me.name, me.reason);
+            }
+        }
+    }};
+    (skip_log, $matcher: expr) => {{
+        use regmock_rs::matchers::*;
+        let mut m = $matcher;
+        match m.r#match(regmock_rs::logs().iter()) {
+            Ok(_) => ..,
+            Err(me) => {
+                panic!("\nFailed to match {} because:\n'{}'", me.name, me.reason);
+            }
+        }
+    }};
     ($log: expr, $matcher: expr) => {{
         use regmock_rs::matchers::*;
         // let log = $log;
@@ -52,8 +78,8 @@ macro_rules! given {
 /// Macro for constructing register specific [`LogMatcher`](crate::matchers::LogMatcher)
 /// structs in a "more prose like" manner.
 ///
-/// All macro variants require a `$target:expr` that evaluates to an
-/// address of a device register in for of a `u64`. The result of this
+/// All macro variants require a `$target:expr` that evaluates to a
+/// register instance. `.addr()` will be called on it. The result of this
 /// expression is used as the `target` register for the constructed
 /// [`LogMatcher`](crate::matchers::LogMatcher).
 ///
@@ -66,28 +92,53 @@ macro_rules! given {
 /// Initializes a [`ReadLastMatcher`](crate::matchers::ReadLastMatcher)
 /// with the value of `$target:expr` as target address.
 ///
+/// ```rust,ignore
+/// require_reg!(pac::PERIPHERAL.register(), read_last)
+/// ```
+///
 /// ## `not_written`
 /// Initializes a [`NotWrittenMatcher`](crate::matchers::NotWrittenMatcher)
 /// with the value of `$target:expr` as target address.
+///
+/// ```rust,ignore
+/// require_reg!(pac::PERIPHERAL.register(), not_written)
+/// ```
 ///
 /// ## `written_once`
 /// Initializes a [`WrittenOnceMatcher`](crate::matchers::WrittenOnceMatcher)
 /// with the value of `$target:expr` as target address.
 ///
+/// ```rust,ignore
+/// require_reg!(pac::PERIPHERAL.register(), written_once)
+/// ```
+///
 /// ## `all_written_before`
 /// Initializes a [`AllWritesBeforeWritesTo`](crate::matchers::AllWritesBeforeWritesTo)
-/// with the value of `$target:expr` as target address and the value of
-/// `$other_address:expr` as the other address.
+/// with the value of `$target:expr` as the target register and
+/// `$other_address:expr` as the other register.
+///
+/// ```rust,ignore
+/// require_reg!(pac::PERIPHERAL.register(), all_written_before(pac::PERIPHERAL.other_register()))
+/// ```
 ///
 /// ## `written_before`
 /// Initializes a [`WrittenToBeforeWriteTo`](crate::matchers::WrittenToBeforeWriteTo)
-/// with the value of `$target:expr` as target address and the value of
-/// `$other_address:expr` as the other address.
+/// with the value of `$target:expr` as the target register and
+/// `$other_address:expr` as the other register.
+///
+/// ```rust,ignore
+/// require_reg!(pac::PERIPHERAL.register(), written_before(pac::PERIPHERAL.other_register()))
+/// ```
 ///
 /// ## `values_written_are`
 /// Initializes a [`ValuesWrittenAre`](crate::matchers::ValuesWrittenAre)
-/// with the value of `$target:expr` as target address and the value of
+/// with the value of `$target:expr` as the target register and
 /// `$sequence:expr` as the sequence of written values.
+/// The values passed will be converted to `u64`s and panic if not possible.
+///
+/// ```rust,ignore
+/// require_reg!(pac::PERIPHERAL.register(), values_written_are([0x11, 0x22, 0x44]))
+/// ```
 #[macro_export]
 macro_rules! require_reg {
     ($target:expr, read_last) => {
@@ -106,15 +157,30 @@ macro_rules! require_reg {
         regmock_rs::matchers::WrittenToBeforeWriteTo::new($target.addr(), $other_address.addr())
     };
     ($target:expr, values_written_are($sequence:expr)) => {
-        regmock_rs::matchers::ValuesWrittenAre::new($target.addr(), $sequence)
+        regmock_rs::matchers::ValuesWrittenAre::new(
+            $target.addr(),
+            $sequence.into_iter().map(|x| {
+                u64::try_from(x)
+                    .inspect_err(|x| {
+                        panic!("Could not convert expected write value to u64 because: {x}")
+                    })
+                    .unwrap()
+            }),
+        )
     };
 }
 
 /// Macro for constructing a [`LogSequenceMatcher`](crate::matchers::LogSequenceMatcher)
 /// struct in a "more prose like" manner.
+/// Works together well with the [`read_access`] and [`write_access`] helpers.
 ///
-/// # Variants and Examples
-/// Construct [`LogSequenceMatcher`](crate::matchers::LogSequenceMatcher).
+/// # Example
+///
+/// ```rust,ignore
+/// let r0 = read_value(pac::PERIPHERAL.register().addr(), 0);
+/// let w0 = write_value(pac::PERIPHERAL.register().addr(), 0x1234);
+/// given!(full_log, require_seq!(vec![&r0, &r1, &w0]));
+/// ```
 #[macro_export]
 macro_rules! require_seq {
     ($seq:expr) => {
